eb6b76d1b409662ee4c2a28b7602ab8b57006d0a
junyoung <adad568@naver.com>
2024-11-16 20:47:33 +0900

    feat(command): messagecommand 클래스 추가

--- src/server/command/MessageCommand.java
+++ src/server/command/MessageCommand.java
@@ -1,4 +1,21 @@
 package server.command;
 
-public class MessageCommand {
+import server.Session;
+import server.SessionManager;
+
+import java.io.IOException;
+
+public class MessageCommand implements Command {
+
+    private final SessionManager sessionManager;
+
+    public MessageCommand(SessionManager sessionManager) {
+        this.sessionManager = sessionManager;
+    }
+
+    @Override
+    public void execute(String[] args, Session session) throws IOException {
+        String message = args[1];
+        sessionManager.sendAll("[" + session.getUsername() + "] " + message);
+    }
 }
